d1a4143f88ac7c5442fca2a1770b6eccdd3f541d
ravelin9 <kvist1495@gmail.com>
2022-08-14 16:12:12 +0500

    contact list added

--- src/pages/event.tsx
+++ src/pages/event.tsx
@@ -1,10 +1,14 @@
 import React, {FC} from 'react';
+import ContactsPage from "../components/ContactsPage";
+import {Layout, Row} from "antd";
 
 const Event: FC = () => {
     return (
-        <div>
-           Event
-        </div>
+        <Layout>
+            <Row justify="center" align="middle">
+                <ContactsPage />
+            </Row>
+        </Layout>
     );
 };
 
