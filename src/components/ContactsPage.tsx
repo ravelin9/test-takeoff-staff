20d0060ef9e18c1cf59cbd36d36642871f48b2df
ravelin9 <kvist1495@gmail.com>
2022-08-14 20:07:26 +0500

    delete contacts function added

--- src/components/ContactsPage.tsx
+++ src/components/ContactsPage.tsx
@@ -1,9 +1,9 @@
 import React, {ChangeEventHandler, FC, useEffect} from 'react';
 import {Avatar, Button, List, Input} from "antd";
-import {fetchContacts} from "../slices/contact/contactApi";
+import {deleteContact, fetchContacts} from "../slices/contact/contactApi";
 import {useDispatch} from "react-redux";
 import {useTypedSelector} from "../hooks/useTypedSelector";
-import {selectContactList} from "../slices/contact/contactSlice";
+import {selectContactList, selectContactStatus} from "../slices/contact/contactSlice";
 
 const { Search } = Input
 
@@ -12,10 +12,13 @@ const ContactsPage: FC = () => {
 
     const contactList = useTypedSelector(selectContactList);
     const dispatch = useDispatch()
+    const status = useTypedSelector(selectContactStatus)
     const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
 
     }
-
+    const handleDeletion = (id: string) => {
+                dispatch(deleteContact(id));
+    };
 
     useEffect(() => {
         dispatch(fetchContacts())
@@ -29,12 +32,15 @@ const ContactsPage: FC = () => {
             className="contactList"
             itemLayout="horizontal"
             dataSource={contactList}
-            renderItem={item => (
-                <List.Item actions={[<Button key="list-loadmore-edit">Изменить</Button>, <Button key="list-loadmore-more" danger>Удалить</Button>]}>
+            loading={status === 'loading'}
+            renderItem={contact => (
+                <List.Item actions={[
+                    <Button key="list-loadmore-edit">Изменить</Button>,
+                    <Button onClick={() => handleDeletion(contact.id)} key="list-loadmore-more" danger>Удалить</Button>]}>
                     <List.Item.Meta
-                        avatar={<Avatar src={item.avatar}/>}
-                        title={item.name}
-                        description={item.phone}
+                        avatar={<Avatar src={contact.avatar}/>}
+                        title={contact.name}
+                        description={contact.phone}
                     />
                 </List.Item>
             )}></List>
